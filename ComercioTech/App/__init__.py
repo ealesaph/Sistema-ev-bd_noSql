9c4478a86a8a4f2a2455a9b815954f4047bf85e3
Juno <juan.navarrete.fritz@gmail.com>
2026-06-28 17:05:35 -0400

    feat: union de coneccion de puerto de frontend a backend

--- ComercioTech/App/__init__.py
+++ ComercioTech/App/__init__.py
@@ -1,10 +1,14 @@
 #Librerias
+import os
 from flask import Flask
 from flask_cors import CORS
 from .config.database_config import init_db
 
 def create_app():
-    app = Flask(__name__)
+    # Ruta a la carpeta dist compilada por Vite
+    frontend_dist = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Frontend', 'static', 'dist')
+    
+    app = Flask(__name__, static_folder=frontend_dist, static_url_path='/')
     CORS(app)
     
     # Configuración de la aplicación
@@ -31,4 +35,18 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(logs_bp, url_prefix='/api/logs')
     
+    # Ruta para servir el frontend de React
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path.startswith('api/'):
+            return "Not Found", 404
+        
+        # Si el archivo existe en la carpeta dist (ej. assets, imagenes), sírvelo
+        if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+            return app.send_static_file(path)
+        # En caso contrario, retorna el index.html para que React Router se encargue
+        else:
+            return app.send_static_file('index.html')
+            
     return app
