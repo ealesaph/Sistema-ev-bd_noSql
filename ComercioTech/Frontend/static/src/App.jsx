0d1acf9bfc3f3c2ab05046a2409ebbfbbf33ba8b
ealesaph <enrique.alegria02@inacapmail.cl>
2026-06-29 23:14:14 -0400

    Landing Page

--- ComercioTech/Frontend/static/src/App.jsx
+++ ComercioTech/Frontend/static/src/App.jsx
@@ -1,12 +1,13 @@
-import MainHeader from './components/header'
-import MainFooter from './components/footer'
-import Carroussel from './components/carrusel'
+import MainHeader from './components/header';
+import MainFooter from './components/footer';
+import Carroussel from './components/carrusel';
+import Landing from './Landing.jsx';
 
 function App() {
   return (
     <>
       <MainHeader />
-      <Carroussel/>
+      <Landing />
       <MainFooter />
     </>
   )
